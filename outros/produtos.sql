04237fece33a20c7f7725f117f7f2787699170dd
vinicius <minervinocva@gmail.com>
2019-11-22 10:01:11 -0300

    adicao sql

---
+++ outros/produtos.sql
@@ -0,0 +1,21 @@
+create database projetoprodutos;
+
+create table tbespecificacao(
+	codigo serial primary key,
+	fabricante varchar(30) not null,
+	cor varchar(30) not null,
+	sistema varchar(30) not null,
+	detalhes varchar(30) not null
+);
+
+
+create table tbproduto(
+	codigo serial primary key,
+	nome varchar(50) not null unique,
+	preco numeric not null,
+	especificacao int not null unique,
+	check(preco>0),
+	foreign key (especificacao) references tbespecificacao(codigo)
+);
+select * from tbespecificacao;
+select * from tbproduto;
